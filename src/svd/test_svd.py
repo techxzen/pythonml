42f19e6f66b9968aaf197f6135c612b6677c04c9
itchencheng <itchencheng@gmail.com>
2018-06-17 16:19:43 +0800

    ok

--- src/svd/test_svd.py
+++ src/svd/test_svd.py
@@ -16,6 +16,21 @@ def loadExData():
             [5, 5, 5, 0, 0]]
 
 
+def loadExData2():
+    data = [[2,0,0,4,4,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,5],
+            [0,0,0,0,0,0,0,1,0,4,0],
+            [3,3,4,0,3,0,0,2,2,0,0],
+            [5,5,5,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,5,0,0,5,0],
+            [4,0,4,0,0,0,0,0,0,0,5],
+            [0,0,0,0,0,4,0,0,0,0,4],
+            [0,0,0,0,0,0,5,0,0,5,0],
+            [0,0,0,3,0,0,0,0,4,5,0],
+            [1,1,2,1,1,2,1,0,4,5,0]]
+    return data
+
+
 def cosSim(inA, inB):
     num   = float(inA.T * inB)
     denom = np.linalg.norm(inA) * np.linalg.norm(inB)
@@ -49,6 +64,29 @@ def standEst(dataMat, user, simMeas, item):
         return ratSimTotal/simTotal
 
 
+def svdEst(dataMat, user, simMeas, item):
+    n = np.shape(dataMat)[1]
+    simTotal = 0.0
+    ratSimTotal = 0.0
+    U, Sigma, VT = np.linalg.svd(dataMat)
+    Sig4 = np.mat(np.eye(4) * Sigma[:4])
+    # print(Sig4)
+    xformedItems = dataMat.T * U[:,:4] * Sig4.I
+    print(xformedItems.shape)
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if (userRating == 0 or j == item):
+            continue
+        similarity = simMeas(xformedItems[item,:].T, xformedItems[j,:].T)
+        # print('the %d and %d similarity is : %f' %(item, j, similarity))
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+        if (simTotal == 0):
+            return 0
+        else:
+            return ratSimTotal / simTotal
+
+
 def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
     unratedItems = np.nonzero(dataMat[user, :].A == 0)[1]
 
@@ -61,6 +99,7 @@ def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
         itemScores.append((item, estimatedScore))
 
     return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[:N]
+
 
 
 def main():
@@ -85,7 +124,14 @@ def main():
     myMat[0,1] = myMat[0,0] = myMat[1,0] = myMat[2,0] = 4
     myMat[3,3] = 2
     # print(myMat)
-    print(recommend(myMat, 2))
+    # print(recommend(myMat, 2))
+
+    ''' recommendation system using SVD '''
+    Data = loadExData2()
+    myMat = np.mat(Data)
+    U, Sigma, V = np.linalg.svd(myMat)
+    print(Sigma)
+    print(recommend(myMat, 1, estMethod=svdEst))
 
 if __name__ == "__main__":
     main()
