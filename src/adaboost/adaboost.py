39e6a39af53005e1611a28013a13a53f4ecef1bf
itchencheng <itchencheng@gmail.com>
2018-08-07 01:54:46 +0800

    base classifier

--- src/adaboost/adaboost.py
+++ src/adaboost/adaboost.py
@@ -15,16 +15,17 @@ import numpy as np
 
 
 def loadSimpData():
-	data = [[1.0,  2.1],
+    data = [[1.0,  2.1],
 			[2.0,  1.1],
 			[1.3,  1.0],
 			[1.0,  1.0],
 			[2.0,  1.0]]
-	label = [1, 1, -1, -1, -1]
+    label = [1, 1, -1, -1, 1]
 	
-	dataSetX = np.array(data)
-	dataSetY = np.array(label)
-	return dataSetX, dataSetY
+    dataSetX = np.array(data)
+    dataSetY = np.array(label)
+    dataSetY = np.reshape(dataSetY, (5,1))
+    return dataSetX, dataSetY
 
 
 def treeClassify(dataArray, dimen, threshold, inEq):
@@ -37,13 +38,17 @@ def treeClassify(dataArray, dimen, threshold, inEq):
         index = dataArray[:,dimen] >= threshold
         ret[index] = -1
 
-    return ret
+    return np.array(ret)
 
 
 def buildTree(dataSetX, dataSetY, dWeight):
     ''' The tree is a single-layer-decision-tree,
         find the most valuable feature, and threshold '''
     m, n = dataSetX.shape
+
+    minError = np.inf
+    bestTree = {}
+
     for i in range(n):
         maxFeatureValue = np.max(dataSetX[:, i])
         minFeatureValue = np.min(dataSetX[:, i])
@@ -51,18 +56,42 @@ def buildTree(dataSetX, dataSetY, dWeight):
         numSteps = 10
         stepSize = (maxFeatureValue - minFeatureValue) / numSteps
 
-        for j in range(-1, np.int(numSteps + 1)):
+        for j in range(-1, np.int(numSteps + 2)):
             threshold = minFeatureValue + np.float(j) * stepSize
 
             for inequal in ['lt', 'gt']:
+                errorArray = np.zeros((m, 1))
                 out = treeClassify(dataSetX, i, threshold, inequal)
-                err = np.
+                print(out)
+                #print(dataSetY)
+                errorArray[out != dataSetY] = 1
+
+                weightedError = np.dot(dWeight.T, errorArray)
+                #print(dWeight.T)
+                #print(errorArray)
+                #print(weightedError)
+                print("split: dim %d, threshold %.2f, inequal: %s -> weightedError: %f" \
+                             %(i, threshold, inequal, weightedError))
+                if (weightedError < minError):
+                    minError = weightedError
+                    bestEst  = out.copy()
+                    bestTree["dim"] = i
+                    bestTree["thresh"] = threshold
+                    bestTree["ineq"] = inequal
+    return bestTree, minError, bestEst
 
 
 def main():
-	dataX, dataY = loadSimpData()
-	print(dataX)
-	print(dataY)
+    dataX, dataY = loadSimpData()
+    print(dataX)
+    print(dataY)
+
+    D = np.ones((5,1)) / 5
+    print(D)
+
+    bestTree, minError, bestEst = buildTree(dataX, dataY, D)
+
+    print((bestTree, minError, bestEst))
 
 
 if __name__ == "__main__":
